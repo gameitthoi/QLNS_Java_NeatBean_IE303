365043780f332f26485ac0115259022ae15e7823
gameitthoi <lamdat3302@gmail.com>
2023-05-12 17:07:35 +0700

    them bang thong ke doanh thu ca nam

--- QLNS/src/main/java/Connect/HoaDon_Connect.java
+++ QLNS/src/main/java/Connect/HoaDon_Connect.java
@@ -8,65 +8,67 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 import Model.HoaDon;
+import java.sql.CallableStatement;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class HoaDon_Connect extends Connect_sqlServer{
     
-	public ArrayList<HoaDon> LayTatCaHoaDon(){
-            ArrayList<HoaDon> dshd = new ArrayList<HoaDon>();
-            try {
-                String sql = "select * from HOADON where ThanhCong=1 and NhapSach=0" ;
-                PreparedStatement pre = conn.prepareStatement(sql);
-                ResultSet result  = pre.executeQuery();
-                while(result.next()){
-                    HoaDon hd = new HoaDon();
-                    hd.setMaHD(result.getString(1));
-                    hd.setMaNV(result.getString(2));
-                    hd.setNgaylap(result.getDate(3)+"");
-                    hd.setTongTien(result.getDouble(4));
-                    dshd.add(hd);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+    public ArrayList<HoaDon> LayTatCaHoaDon(){
+        ArrayList<HoaDon> dshd = new ArrayList<HoaDon>();
+        try {
+            String sql = "select * from HOADON where ThanhCong=1 and NhapSach=0" ;
+            PreparedStatement pre = conn.prepareStatement(sql);
+            ResultSet result  = pre.executeQuery();
+            while(result.next()){
+                HoaDon hd = new HoaDon();
+                hd.setMaHD(result.getString(1));
+                hd.setMaNV(result.getString(2));
+                hd.setNgaylap(result.getDate(3)+"");
+                hd.setTongTien(result.getDouble(4));
+                dshd.add(hd);
             }
-            return dshd ;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
-	public ArrayList<HoaDon> layToanBoHoaDonTheoThangNam(String month , String year)
-	{
-            ArrayList<HoaDon> dshd = new ArrayList<HoaDon>();
-            String sql = "";
-            try {
-                if("0".equals(month))
-                    sql = "select * from HOADON where Year(NgayLap) = ? and ThanhCong = 1 and NhapSach=0" ;
-                else
-                    sql = "select * from HOADON where MONTH(NgayLap) = ? and Year(NgayLap) = ? and ThanhCong = 1 and NhapSach=0" ;
+        return dshd ;
+    }
 
-                PreparedStatement pre = conn.prepareStatement(sql);
+    public ArrayList<HoaDon> layToanBoHoaDonTheoThangNam(String month , String year)
+    {
+        ArrayList<HoaDon> dshd = new ArrayList<HoaDon>();
+        String sql = "";
+        try {
+            if("0".equals(month))
+                sql = "select * from HOADON where Year(NgayLap) = ? and ThanhCong = 1 and NhapSach=0" ;
+            else
+                sql = "select * from HOADON where MONTH(NgayLap) = ? and Year(NgayLap) = ? and ThanhCong = 1 and NhapSach=0" ;
 
-                if ("0".equals(month))
-                    pre.setString(1, year);
-                else {
-                    pre.setString(1,month);
-                    pre.setString(2, year);
-                }
-                ResultSet result  = pre.executeQuery();
-                while(result.next())
-                {
-                    HoaDon hd = new HoaDon();
-                    hd.setMaHD(result.getString(1));
-                    hd.setMaNV(result.getString(2));
-                    hd.setNgaylap(result.getDate(3)+"");
-                    hd.setTongTien(result.getDouble(4));
-                    dshd.add(hd);
-                }
+            PreparedStatement pre = conn.prepareStatement(sql);
 
-            } catch (Exception e) {
-                    e.printStackTrace();
+            if ("0".equals(month))
+                pre.setString(1, year);
+            else {
+                pre.setString(1,month);
+                pre.setString(2, year);
             }
-            return dshd ;
-	}
+            ResultSet result  = pre.executeQuery();
+            while(result.next())
+            {
+                HoaDon hd = new HoaDon();
+                hd.setMaHD(result.getString(1));
+                hd.setMaNV(result.getString(2));
+                hd.setNgaylap(result.getDate(3)+"");
+                hd.setTongTien(result.getDouble(4));
+                dshd.add(hd);
+            }
+
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        return dshd ;
+    }
 	
-	public String LastMaHD(){
+    public String LastMaHD(){
         try{
             //String sql = "select * from hoadon ORDER BY mahd DESC LIMIT 1" ;
             String sql = "SELECT TOP 1 * FROM hoadon ORDER BY mahd DESC";
@@ -76,9 +78,25 @@ public class HoaDon_Connect extends Connect_sqlServer{
         }
         catch (Exception e) {
             e.printStackTrace();
-	}
-                        
+        }
+
         return null;
+    }
+    
+    public DefaultCategoryDataset DoanhThuCacThang(){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        try{
+            String sql = "{call DoanhThuCacThang()}";
+             CallableStatement  stmt  = conn.prepareCall(sql);		
+            ResultSet result = stmt.executeQuery();
+            while(result.next()){
+                dataset.addValue( Double.parseDouble(result.getString(2)),"Doanh thu bán sách",result.getString(1));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataset;
     }
 
     public int TaoHD(HoaDon hd) {
